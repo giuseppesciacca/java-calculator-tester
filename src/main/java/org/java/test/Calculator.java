d67f87ffc19039ba875adaf87954c430a4d9644f
giuseppesciacca <sciaccagi@gmail.com>
2023-09-29 15:16:32 +0200

    ADD Calculator class and test

--- src/main/java/org/java/test/Calculator.java
+++ src/main/java/org/java/test/Calculator.java
@@ -9,7 +9,11 @@ public class Calculator {
 		return num1 - num2;
 	}
 
-	public float divide(float num1, float num2) {
+	public float divide(float num1, float num2) throws Exception {
+
+		if (num2 == 0) {
+			throw new Exception("Impossibile dividere per zero");
+		}		
 		return num1 / num2;
 	}
 
--- src/test/java/org/java/test/CalculatorTest.java
+++ src/test/java/org/java/test/CalculatorTest.java
@@ -1,5 +1,6 @@
 package org.java.test;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -13,13 +14,43 @@ public class CalculatorTest {
 	@BeforeEach
 	public void beforeEach() {
 		calculator = new Calculator();
-
 	}
 
 	@Test
 	@DisplayName("Test add operation")
-	public void addOperation() {
+	public void testAdd() {
 
 		assertEquals(10, calculator.add(7, 3));
+	}
+
+	@Test
+	@DisplayName("Test subtract operation")
+	public void testSubtract() {
+
+		assertEquals(10, calculator.subtract(13, 3));
+	}
+
+	@Test
+	@DisplayName("Test divide operation")
+	public void testDivide() throws Exception {
+
+		try {
+			assertEquals(10, calculator.divide(20, 2));
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+	@Test
+	@DisplayName("Test divide /0")
+	void testDividi() {
+		assertDoesNotThrow(() -> calculator.divide(10, 0), "Deve sollevare IllegalArgumentException per divisione per zero");
+	}
+
+	@Test
+	@DisplayName("Test multiply operation")
+	public void testMultiply() {
+
+		assertEquals(10, calculator.multiply(5, 2));
 	}
 }
